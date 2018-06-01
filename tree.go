0dd84648b46b721a05111d3b5a1375c296ceebcb
Jaden Weiss <jadr2ddude@gmail.com>
2018-05-31 22:30:32 -0400

    try ditching multierror

--- tree.go
+++ tree.go
@@ -223,7 +223,7 @@ func (tb *treeBuilder) checkCycle(parent *cycleChain, jt *jTree) error {
 			}
 			e = tb.checkCycle(cc, dt)
 			if e != nil {
-				errs = append(errs, e)
+				return e
 			}
 		}
 	}
