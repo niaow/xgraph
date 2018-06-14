f573e8e57f52cdd5c249aeff474563783bb9b0d5
Jaden Weiss <jadr2ddude@gmail.com>
2018-06-14 08:42:42 -0400

    implement fast dep cycle detector

--- tree.go
+++ tree.go
@@ -2,7 +2,6 @@ package xgraph
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 )
 
@@ -118,46 +117,6 @@ func (tb *treeBuilder) genTree(name string) (*jTree, error) {
 	return t, nil
 }
 
-// listContains checks if a sorted list contains a value
-func listContains(list []string, val string) bool {
-	i := sort.SearchStrings(list, val)
-	if i == len(list) {
-		return false
-	}
-	return list[i] == val
-}
-
-type cycleChain struct {
-	name   string
-	parent *cycleChain
-}
-
-// jTreeNames takes a slice of *jTree and returns a sorted slice of the names
-func jTreeNames(trees []*jTree) []string {
-	names := make([]string, len(trees))
-	for i, v := range trees {
-		names[i] = v.name
-	}
-	sort.Strings(names)
-	return names
-}
-
-func names2map(names []string) map[string]struct{} {
-	m := make(map[string]struct{})
-	for _, n := range names {
-		m[n] = struct{}{}
-	}
-	return m
-}
-
-// sub returns a cycleChain with this cycleChain as the parent
-func (cc *cycleChain) sub(name string) *cycleChain {
-	return &cycleChain{
-		name:   name,
-		parent: cc,
-	}
-}
-
 // DependencyCycleError is an error indicating that there is a dependency cycle
 type DependencyCycleError []string
 
@@ -165,52 +124,89 @@ func (dce DependencyCycleError) Error() string {
 	return "dependency cycle: " + strings.Join([]string(dce), "->")
 }
 
-// check recurses the *cycleChain to check if there is a dependency cycle with the given name
-func (cc *cycleChain) check(name string) error {
-	if cc.name == name {
-		return DependencyCycleError{name, cc.name}
-	}
-	if cc.parent == nil {
-		return nil
-	}
-	cyc := cc.parent.check(name)
-	if cyc != nil {
-		return append(cyc.(DependencyCycleError), cc.name)
-	}
-	return nil
+type backStack []int
+
+func (bs *backStack) push(val int) {
+	*bs = append(*bs, val)
 }
 
-// chainRoot is a *cycleChain which has no parent
-var chainRoot = &cycleChain{
-	name:   "",
-	parent: nil,
+func (bs *backStack) drop() {
+	*bs = (*bs)[:len(*bs)-1]
 }
 
-func (tb *treeBuilder) checkCycle(parent *cycleChain, jt *jTree) error {
-	if err := parent.check(jt.name); err != nil {
-		return err
-	}
-	sub := parent.sub(jt.name)
-	for _, v := range jt.deps {
-		err := tb.checkCycle(sub, v)
-		if err != nil {
-			return err
+func (bs *backStack) search(val int) bool {
+	for _, v := range *bs {
+		if v == val {
+			return true
 		}
 	}
-	return nil
+	return false
+}
+
+type depIndex [][]int
+
+func (di depIndex) checkDeps(node int, bs *backStack) (int, bool) {
+	if node == -1 {
+		return -1, true
+	}
+	if bs.search(node) {
+		bs.push(node)
+		return node, true
+	}
+	bs.push(node)
+	for _, v := range di[node] {
+		if n, cyc := di.checkDeps(v, bs); cyc {
+			return n, true
+		}
+	}
+	bs.drop()
+	return -1, false
 }
 
 func (tb *treeBuilder) findCycles() []*jTree {
-	cTrees := []*jTree{}
-	for _, v := range tb.forest {
-		err := tb.checkCycle(chainRoot, v)
-		if err != nil {
-			if v.err == nil {
-				v.err = err
-			}
-			cTrees = append(cTrees, v)
-		}
+	//generate string-int mapping tables
+	s2n := make(map[string]int)
+	n2s := []string{}
+	for n := range tb.forest {
+		i := len(n2s)
+		s2n[n] = i
+		n2s = append(n2s, n)
 	}
+
+	//generate dep index
+	di := make(depIndex, len(tb.forest))
+	for _, v := range tb.forest {
+		i := s2n[v.name]
+		dlst := make([]int, len(v.deps))
+		for j, k := range v.deps {
+			d, exist := s2n[k.name]
+			if !exist {
+				d = -1
+			}
+			dlst[j] = d
+		}
+		di[i] = dlst
+	}
+
+	//scan with index
+	cTrees := []*jTree{}
+	bs := new(backStack)
+	for i := range di {
+		n, cyc := di.checkDeps(i, bs)
+		if cyc && n != -1 {
+			t := tb.forest[n2s[n]]
+			if t.err == nil {
+				c := make([]string, len(*bs))
+				for i, v := range *bs {
+					c[i] = n2s[v]
+				}
+				t.err = DependencyCycleError(c)
+				cTrees = append(cTrees, t)
+			}
+		}
+		*bs = (*bs)[:0]
+	}
+
 	if len(cTrees) > 0 {
 		return cTrees
 	}
